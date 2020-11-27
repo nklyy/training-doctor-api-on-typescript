9d9746e1424f4752a8b95ecfe9e2c2208dbe7de5
Nikolay <44751173+nklyy@users.noreply.github.com>
2020-11-27 12:55:50 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, {Request, Response} from 'express';
+
+const app = express();
+
+
+app.use('/', (req: Request, res: Response) => {
+    res.send('Hi');
+});
+
+app.listen(3000, () => {
+    console.log('123');
+});
